1a41c757ddf0c1c7281a352926d3db2713640d5c
Sandesh1998 <sandeshpoudel144@gmail.com>
2022-12-27 20:21:30 +0545

    UI desgining

--- app/src/main/java/com/example/khojam/Models/Definitions.java
+++ app/src/main/java/com/example/khojam/Models/Definitions.java
@@ -1,8 +1,28 @@
 package com.example.khojam.Models;
 
+import java.util.List;
+
 public class Definitions {
     String definition = "";
     String example = "";
+    List<String> synonyms = null;
+    List<String> antonyms = null;
+
+    public List<String> getSynonyms() {
+        return synonyms;
+    }
+
+    public void setSynonyms(List<String> synonyms) {
+        this.synonyms = synonyms;
+    }
+
+    public List<String> getAntonyms() {
+        return antonyms;
+    }
+
+    public void setAntonyms(List<String> antonyms) {
+        this.antonyms = antonyms;
+    }
 
     public String getDefinition() {
         return definition;
