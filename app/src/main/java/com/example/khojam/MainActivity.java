87b8f93a22cdf50690b10858a950c415cdccd312
Sandesh1998 <sandeshpoudel144@gmail.com>
2022-12-30 14:46:06 +0545

    On Fetch data is completed

--- app/src/main/java/com/example/khojam/MainActivity.java
+++ app/src/main/java/com/example/khojam/MainActivity.java
@@ -2,15 +2,25 @@ package com.example.khojam;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.khojam.Adapters.MeaningAdapter;
+import com.example.khojam.Adapters.PhoneticsAdapter;
+import com.example.khojam.Models.APIResponse;
 
 public class MainActivity extends AppCompatActivity {
     SearchView search_view;
     TextView textView_word;
     RecyclerView recycler_phonetics,recycler_meanings;
+    ProgressDialog progressDialog;
+    PhoneticsAdapter phoneticsAdapter;
+    MeaningAdapter meaningAdapter;
 
 
     @Override
@@ -22,6 +32,56 @@ public class MainActivity extends AppCompatActivity {
         textView_word = findViewById(R.id.textView_word);
         recycler_phonetics = findViewById(R.id.recycler_phonetics);
         recycler_meanings = findViewById(R.id.recycler_meanings);
+        progressDialog = new ProgressDialog(this);
+
+
+        search_view.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                progressDialog.setTitle("Fetching response for" + query);
+                progressDialog.show();
+                RequestManager manager = new RequestManager(MainActivity.this);
+                manager.getWordMeaning(listener, query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+    }
+    private final OnFetchDataListener listener = new OnFetchDataListener() {
+        @Override
+        public void onFetchData(APIResponse apiResponse, String message) {
+            progressDialog.dismiss();
+            if(apiResponse==null){
+                Toast.makeText(MainActivity.this, "No data found!!!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            showData(apiResponse);
+
+        }
+
+        @Override
+        public void onError(String message) {
+
+        }
+    };
+
+    private void showData(APIResponse apiResponse) {
+        textView_word.setText("word:" + apiResponse.getWord());
+        recycler_phonetics.setHasFixedSize(true);
+        recycler_phonetics.setLayoutManager(new GridLayoutManager(this,1));
+        phoneticsAdapter = new PhoneticsAdapter(this,apiResponse.getPhonetics());
+        recycler_phonetics.setAdapter(phoneticsAdapter);
+
+        recycler_meanings.setHasFixedSize(true);
+        recycler_meanings.setLayoutManager(new GridLayoutManager(this, 1));
+        meaningAdapter = new MeaningAdapter(this, apiResponse.getMeanings());
+        recycler_meanings.setAdapter(meaningAdapter);
+
 
 
     }
